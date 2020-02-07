bc1611b9c13ad51ee48d6d535fb4090eb4d2f9b5
Stefano Saffran <stefanosaffran@hotmail.com>
2020-02-07 11:25:58 +0100

    feat: initial commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StatusBar, YellowBox } from 'react-native';
+import Routes from '~/routes';
+import Player from '~/components/Player';
+
+YellowBox.ignoreWarnings([
+  'Unrecognized WebSocket',
+  'VirtualizedLists should never be nested', // TODO: Remove when fixed
+]);
+
+export default function App() {
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#111" />
+      <Routes />
+      <Player />
+    </>
+  );
+}
